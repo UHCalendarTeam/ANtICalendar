9909e8d60188bb45dfa68d5fef59e63ed4eb0f70
Adriano Flechilla <a.flechilla91@gmail.com>
2016-02-09 13:05:48 -0500

    -Added 2 interfaces.

---
+++ ICalendar/ComponentProperties/Attach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICalendar.ComponentProperties
+{
+    public class Attach : IComponentProperty, ISerialize
+    {
+        public string Name
+        {
+            get{ return "ATTACH";}          
+        }
+
+        IComponentProperty ISerialize.Deserialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISerialize.Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
