a442e3ec7b1420c982008bb91cae794e2f8681d3
Adriano Flechilla <a.flechilla91@gmail.com>
2016-02-22 18:01:02 -0500

    - Added the classes for the Relationship Properties.

---
+++ src/ICalendar/ComponentProperties/Relationship/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICalendar.ComponentProperties
+{
+    /// <summary>
+    /// Purpose: This property is used to represent contact information or
+    ///alternately a reference to contact information associated with the
+    ///calendar component.
+    ///Conformance: This property can be specified in a "VEVENT", "VTODO",
+    ///"VJOURNAL", or "VFREEBUSY" calendar component.
+    /// </summary>
+    public class Contact : ComponentProperty<string>
+    {
+        public override string Name => "CONTACT";
+    }
+}
