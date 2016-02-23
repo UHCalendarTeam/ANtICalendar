8da2accdf7be82b1517fa619b40ab6f2b76e8185
NachoVazquez <nachovc1410@gmail.com>
2016-02-23 12:45:50 -0500

    Starting with Recur

---
+++ src/ICalendar/ValueTypes/Recur.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICalendar.ValueTypes
+{
+    public enum Frequencies
+    {
+        Minutely, Hourly, Daily, Weekly, Monthly, Yearly
+    }
+    public class Recur
+    {
+        public Frequencies Frequency { get; set; }
+
+        public DateTime Until { get; set; }
+
+        public int Count { get; set; }
+
+        public int Interval { get; set; }
+
+        public int [] BySeconds { get; set; }
+
+        public int [] ByMinutes { get; set; }
+
+        public int [] ByHours { get; set; }
+
+        public int [] ByDays { get; set; }
+
+
+    }
+}
